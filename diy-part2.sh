1385fcbd3d05b8224211d7c0d81a36310504c485
michael8278769 <42510449+michael8278769@users.noreply.github.com>
2021-09-08 15:39:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,4 +16,4 @@
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/OpenWrt/OpeWRT/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/OpenWrt/OpeWRT/g' package/lean/default-settings/files/zzz-default-settins
