b465a2ecbcef9bd5be39ec9e8e61ae5e1113dd17
michael8278769 <42510449+michael8278769@users.noreply.github.com>
2021-09-08 01:17:34 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/OpeWRT/g' package/lean/default-settings/files/zzz-default-settings
